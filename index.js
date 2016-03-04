b64c0388ddc968c0911f3b4452295e8a70099fde
Dongkwan Kim <0xdkay@gmail.com>
2016-03-04 16:34:16 +0900

    my resume

--- index.js
+++ index.js
@@ -18,22 +18,22 @@ function interpolate(object, keyPath) {
 function capitalize(str) {
     if (str) {
         str = str.toString();
-        return str[0].toUpperCase() + str.slice(1).toLowerCase();
-    }
+        return str[0].toUpperCase() + str.slice(1).toLowerCase(); }
 
     return str;
 }
 
 function getFloatingNavItems(resume) {
     var floating_nav_items = [
-        {label: 'About', target: 'about', icon: 'board', requires: 'basics.summary'},
-        {label: 'Work Experience', target: 'work-experience', icon: 'office', requires: 'work'},
-        {label: 'Skills', target: 'skills', icon: 'tools', requires: 'skills'},
+        {label: 'Bio', target: 'about', icon: 'board', requires: 'basics.summary'},
+        {label: 'Research Interests', target: 'interests', icon: 'heart', requires: 'interests'},
         {label: 'Education', target: 'education', icon: 'graduation-cap', requires: 'education'},
-        {label: 'Awards', target: 'awards', icon: 'trophy', requires: 'awards'},
+        {label: 'Work Experience', target: 'work-experience', icon: 'office', requires: 'work'},
         {label: 'Volunteer Work', target: 'volunteer-work', icon: 'child', requires: 'volunteer'},
+        {label: 'Awards', target: 'awards', icon: 'trophy', requires: 'awards'},
+        {label: 'Projects', target: 'projects', icon: 'list', requires: 'projects'},
+        {label: 'Skills', target: 'skills', icon: 'tools', requires: 'skills'},
         {label: 'Publications', target: 'publications', icon: 'newspaper', requires: 'publications'},
-        {label: 'Interests', target: 'interests', icon: 'heart', requires: 'interests'},
         {label: 'References', target: 'references', icon: 'thumbs-up', requires: 'references'}
     ];
 
@@ -71,6 +71,24 @@ function render(resume) {
     resume.basics.top_five_profiles = resume.basics.profiles.slice(0, 5);
     resume.basics.remaining_profiles = resume.basics.profiles.slice(5);
 
+
+    _.each(resume.education, function(education_info) {
+        _.each(['startDate', 'endDate'], function(type) {
+            var date = education_info[type];
+
+            if (date) {
+                education_info[type] = utils.getFormattedDate(date);
+
+                var end_date = new Date(date);
+                var current_date = new Date();
+
+                if (end_date > current_date) {
+                  education_info[type] += " (expected)";
+                }
+            }
+        });
+    });
+
     _.each(resume.work, function(work_info) {
         var duration;
         var start_date = work_info.startDate;
@@ -94,31 +112,26 @@ function render(resume) {
         }
     });
 
-    _.each(resume.skills, function(skill_info) {
-        var levels = ['Beginner', 'Intermediate', 'Advanced', 'Master'];
+    _.each(resume.projects, function(project_info) {
+        var duration;
+        var start_date = project_info.startDate;
+        var end_date = project_info.endDate;
+        var did_leave_company = !!end_date;
 
-        if (skill_info.level) {
-            skill_info.skill_class = skill_info.level.toLowerCase();
-            skill_info.level = capitalize(skill_info.level.trim());
-            skill_info.display_progress_bar = _.contains(levels, skill_info.level);
+        if (end_date) {
+            project_info.endDate = utils.getFormattedDate(end_date);
         }
-    });
 
-    _.each(resume.education, function(education_info) {
-        _.each(['startDate', 'endDate'], function(type) {
-            var date = education_info[type];
+        if (start_date) {
+            end_date = end_date || new Date();
+            duration = utils.getDuration(start_date, end_date);
+            project_info.startDate = utils.getFormattedDate(start_date);
 
-            if (date) {
-                education_info[type] = utils.getFormattedDate(date);
+            if (!duration.years() && !duration.months() && duration.days() > 1) {
+                project_info.duration = 'Recently joined';
+            } else {
+                project_info.duration = utils.getDuration(start_date, end_date, true);
             }
-        });
-    });
-
-    _.each(resume.awards, function(award) {
-        var date = award.date;
-
-        if (date) {
-            award.date = utils.getFormattedDate(date, 'MMM DD, YYYY');
         }
     });
 
@@ -132,12 +145,32 @@ function render(resume) {
         });
     });
 
+    _.each(resume.skills, function(skill_info) {
+        var levels = ['Beginner', 'Intermediate', 'Advanced', 'Master'];
+
+        if (skill_info.level) {
+            skill_info.skill_class = skill_info.level.toLowerCase();
+            skill_info.level = capitalize(skill_info.level.trim());
+            skill_info.display_progress_bar = _.contains(levels, skill_info.level);
+        }
+    });
+
+    _.each(resume.awards, function(award) {
+        var date = award.date;
+
+        if (date) {
+            award.date = utils.getFormattedDate(date, 'MMM DD, YYYY');
+        }
+    });
+
     _.each(resume.publications, function(publication_info) {
         var date = publication_info.releaseDate;
 
         if (date) {
             publication_info.releaseDate = utils.getFormattedDate(date, 'MMM DD, YYYY');
         }
+
+        publication_info.author = publication_info.author.replace("Dongkwan Kim", "<b>Dongkwan Kim</b>")
     });
 
     return jade.renderFile(__dirname + '/index.jade', {
